307057ea80da702fdf1d68cf4aaeb20fd22ddcab
Eduard <stan.i.eduard@gmail.com>
2021-04-27 10:45:28 +0200

    added tests for discrete_derivatives

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "SymbolicML.jl" begin
     # Write your tests here.
+    x = [-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0]
+    dd = discrete_derivatives(x; d=1)
+    @test dd[:,1] == x
+    @test dd[:,2] == [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0]
 end
